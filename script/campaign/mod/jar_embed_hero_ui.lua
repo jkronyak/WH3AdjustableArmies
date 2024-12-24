2b8f909d5a93c02a981b18b2f9952185b258a494
jkronyak <56709613+jkronyak@users.noreply.github.com>
2024-12-24 18:16:55 -0500

    Added support for unit_purchasable_effects upgrades, such as scrap

--- script/campaign/mod/jar_embed_hero_ui.lua
+++ script/campaign/mod/jar_embed_hero_ui.lua
@@ -1,13 +1,12 @@
 --[[
-TODO: 
-* Add get_unit_purchased_effects, 
-    cm:faction_purchase_unit_effect(faction faction, unit unit, unit_purchasable_effect purchasable effect)
-
-CHECKS:
-* ancillaries (i.e. banners, such as Da Immortulz)
-    * Lord keeps the ancillary [GOOD]
 
 ]]--
+local tables = require('data.table_data')
+
+local unit_purchasable_effect_mapping_grn = tables.unit_purchasable_effect_mapping_grn
+local unit_purchasable_effect_mapping_wef = tables.unit_purchasable_effect_mapping_wef
+local unit_purchasable_effect_mapping_throt_aug = tables.unit_purchasable_effect_mapping_throt_aug
+local unit_purchasable_effect_mapping_throt_ins =tables.unit_purchasable_effect_mapping_throt_ins
 
 local hero_to_embed = nil
 
@@ -68,7 +67,6 @@ end
 -- Stores the currently selected non-lord character into a variable
 local function handle_hero_selected()
     local char = cm:get_character_by_cqi(cm:get_campaign_ui_manager():get_char_selected_cqi())
-
     -- Only set hero_to_embed if the character is not embedded, and is not a lord
     if not (char:is_embedded_in_military_force() or char:has_military_force()) then
         out("Char is not embedded, and is not a general.")
@@ -79,6 +77,8 @@ end
 -- Embeds hero_to_embed into the selected army by performing a 'refresh' of the units
 local function handle_selected_army(context)
     local lord = context:character()
+    local faction = lord:faction()
+    out(cm:are_pooled_resources_tracked_for_faction(faction:name()))
     -- If there is no hero_to_embed, return.
     if hero_to_embed == nil then
         return
@@ -99,6 +99,7 @@ local function handle_selected_army(context)
                 strength =  cur:percentage_proportion_of_full_strength(),
                 purchased_effects = cur:get_unit_purchased_effects()
             })
+
             cm:remove_unit_from_character(
                 cm:char_lookup_str(mf:general_character()),
                 cur:unit_key()
@@ -111,16 +112,98 @@ local function handle_selected_army(context)
 
     -- Add the units back into the army, and set their experience and HP at each step
     local num_units = mf:unit_list():num_items()
-    for i, unit in ipairs(units_to_re_add) do
+    for i, og_unit in ipairs(units_to_re_add) do
         cm:grant_unit_to_character(
             cm:char_lookup_str(mf:general_character()),
-            unit.key
+            og_unit.key
         )
-        local u = mf:unit_list():item_at(i + num_units - 1)
-        if(unit.experience_level ~= 0) then
-            cm:add_experience_to_unit(u, unit.experience_level)
-            cm:set_unit_hp_to_unary_of_maximum(u, unit.strength / 100)
+        local new_unit = mf:unit_list():item_at(i + num_units - 1)
+        if og_unit.strength ~= 100 then
+            cm:set_unit_hp_to_unary_of_maximum(new_unit, og_unit.strength / 100)
         end
+        if og_unit.experience_level ~= 0 then
+            cm:add_experience_to_unit(new_unit, og_unit.experience_level)
+        end
+
+        if og_unit.purchased_effects:num_items() > 0 then
+
+            -- console_print("**"..og_unit.key)
+
+            for j=0, og_unit.purchased_effects:num_items() - 1 do
+                local cur = og_unit.purchased_effects:item_at(j)
+                local cur_key = cur:record_key()
+
+                -- If the unit_purchasable_effect is a green skin scrap upgrade
+                if unit_purchasable_effect_mapping_grn[cur_key] then
+                    local mapping = unit_purchasable_effect_mapping_grn
+                    local factor = mapping[cur_key].pooled_resource_factor
+                    local amount = -tonumber(mapping[cur_key].pooled_resource_amount) + (30 * j) -- Each scrap upgrade adds +30 to the cost
+                    local resource = mapping[cur_key].pooled_resource
+                    -- console_print("**"..j.."| "..cur_key.."| "..factor.."| "..amount.."| "..resource)
+    
+                    local spent_b, gained_b = cm:get_total_pooled_resource_changed_for_faction(faction:name(), resource, factor)
+                    local before_amount = gained_b - spent_b
+                    cm:faction_add_pooled_resource(faction:name(), resource, factor, amount)
+                    cm:faction_purchase_unit_effect(faction, new_unit, cur)
+    
+                    local spent_a, gained_a = cm:get_total_pooled_resource_changed_for_faction(faction:name(), resource, factor)
+                    local after_amount = gained_a - spent_a
+                    local correction_amount = -(after_amount - before_amount)
+                    cm:faction_add_pooled_resource(faction:name(), resource, factor, correction_amount)
+                                    
+                elseif unit_purchasable_effect_mapping_throt_aug[cur_key] then
+                    local mapping = unit_purchasable_effect_mapping_throt_aug
+                    local factor = mapping[cur_key].pooled_resource_factor
+                    local amount = -tonumber(mapping[cur_key].pooled_resource_amount)
+                    local resource = mapping[cur_key].pooled_resource
+                    -- console_print("**"..j.."| "..cur_key.."| "..factor.."| "..amount.."| "..resource)
+    
+                    local spent_b, gained_b = cm:get_total_pooled_resource_changed_for_faction(faction:name(), resource, factor)
+                    local before_amount = gained_b - spent_b
+                    cm:faction_add_pooled_resource(faction:name(), resource, factor, amount)
+                    cm:faction_purchase_unit_effect(faction, new_unit, cur)
+    
+                    local spent_a, gained_a = cm:get_total_pooled_resource_changed_for_faction(faction:name(), resource, factor)
+                    local after_amount = gained_a - spent_a
+                    local correction_amount = -(after_amount - before_amount)
+                    cm:faction_add_pooled_resource(faction:name(), resource, factor, correction_amount)
+                elseif unit_purchasable_effect_mapping_wef[cur_key] then
+                    local amount = -tonumber(unit_purchasable_effect_mapping_wef[cur_key].treasury_amount)
+                    cm:treasury_mod(faction:name(), amount)
+                    cm:faction_purchase_unit_effect(faction, new_unit, cur)
+
+                end
+                
+            end
+
+            -- Iterate the purchased_effects again, this time only checking for Throt's instabilities
+            -- They must be added last, otherwise they increase the chance of further instability for every augment added.
+            for j=0, og_unit.purchased_effects:num_items() - 1 do
+                local cur = og_unit.purchased_effects:item_at(j)
+                local cur_key = cur:record_key()
+                if unit_purchasable_effect_mapping_throt_ins[cur_key] then
+                    local mapping = unit_purchasable_effect_mapping_throt_ins
+                    local factor = mapping[cur_key].pooled_resource_factor
+                    local amount = -tonumber(mapping[cur_key].pooled_resource_amount)
+                    local resource = mapping[cur_key].pooled_resource
+                    -- console_print("**"..j.."| "..cur_key.."| "..factor.."| "..amount.."| "..resource)
+    
+                    local spent_b, gained_b = cm:get_total_pooled_resource_changed_for_faction(faction:name(), resource, factor)
+                    local before_amount = gained_b - spent_b
+                    cm:faction_add_pooled_resource(faction:name(), resource, factor, amount)
+                    cm:faction_purchase_unit_effect(faction, new_unit, cur)
+    
+                    local spent_a, gained_a = cm:get_total_pooled_resource_changed_for_faction(faction:name(), resource, factor)
+                    local after_amount = gained_a - spent_a
+                    local correction_amount = -(after_amount - before_amount)
+                    cm:faction_add_pooled_resource(faction:name(), resource, factor, correction_amount)
+                    
+                end
+            end
+            
+        end
+
+
     end
 
     -- 'Deselect' the hero_to_embed
@@ -142,7 +225,7 @@ core:add_listener(
 
 core:add_listener(
     "JarAdjArmiesArmySelected",
-    "CharacterSelected", 
+    "CharacterSelected",
     function(context)
         local char = context:character()
         return char:faction():is_human() and char:has_military_force()
@@ -159,4 +242,15 @@ core:add_listener(
     end,
     handle_hero_selected,
     true
+)
+
+cm:add_first_tick_callback(function()
+    local human_faction_keys = cm:get_human_factions()
+    for _, key in ipairs(human_faction_keys) do
+        if cm:are_pooled_resources_tracked_for_faction(key) then
+            return
+        end
+        cm:start_pooled_resource_tracker_for_faction(key)
+    end
+end
 )
