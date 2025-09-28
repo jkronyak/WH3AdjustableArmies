72fdec0943915ad7fcee3031f47ecf8e18a3126e
jkronyak <56709613+jkronyak@users.noreply.github.com>
2025-09-28 12:38:38 -0400

    updated for 6.3.1; added infinite siege fix

--- mod/script/campaign/mod/jar_adjustable_armies.lua
+++ mod/script/campaign/mod/jar_adjustable_armies.lua
@@ -5,7 +5,7 @@ Known Issues:
 *   Units do not retain their progress to the next experience level.
 ]] --
 -- [Settings] --
-local settings = {army_size = 40, auto_refresh = true, dev_logging = false, ai_army_size = 40}
+local settings = {army_size = 40, auto_refresh = true, dev_logging = false, ai_army_size = 40, force_siege_assault = true, force_siege_assault_turns = 3}
 
 -- [Logging] --
 local log_override = false
@@ -41,6 +41,8 @@ core:add_listener(
         settings.ai_army_size = my_mod:get_option_by_key("ai_army_size"):get_finalized_setting()
         settings.auto_refresh = my_mod:get_option_by_key("auto_refresh"):get_finalized_setting()
         settings.dev_logging = my_mod:get_option_by_key("dev_logging"):get_finalized_setting()
+        settings.force_siege_assault = my_mod:get_option_by_key("force_siege_assault"):get_finalized_setting()
+        settings.force_siege_assault_turns = my_mod:get_option_by_key("force_siege_assault_turns"):get_finalized_setting()
     end, true
 )
 
@@ -60,8 +62,8 @@ function init_addresses()
     Log("[FUNC] init_addresses")
     new_army_limit = mr.uint32(settings.army_size)
     ai_new_army_limit = mr.uint32(settings.ai_army_size)
-    player_army_size_offset = 0x20182BC
-    exchange_panel_ui_offset = 0x1FD8CFC
+    exchange_panel_ui_offset = 0x208C09C
+    player_army_size_offset = 0x20CB64C
     ai_army_size_offset = player_army_size_offset + 0x10
     -- unknown_20_offset = exchange_panel_ui_offset + 0x10
 end
@@ -350,6 +352,74 @@ local function handle_army_selected(context)
     refresh_army_with_hero(context:character(), hero)
 end
 
+local function handle_char_besieges(region)
+    Log("[FUNC] handle_char_besieges", region:name())
+    local besieging_char = region:garrison_residence():besieging_character()
+    local cur_siege_tbl = cm:get_saved_value('jar_siege_tbl')
+
+    local f = besieging_char:faction():name()
+    local turn_num = cm:turn_number()
+    local besieger_cqi = besieging_char:cqi()
+
+    -- If cur_siege_tbl is not set, then simply add the faction and the siege entry
+    -- Case 1: table does not exist, so add the faction and siege entry
+    if cur_siege_tbl == nil then
+        Log("cur_siege_tbl is nil")
+        local tbl = { [f] = { [besieger_cqi] = { ["turn_started"] = turn_num, ["target_region"] = region:name() } } }
+        cm:set_saved_value('jar_siege_tbl', tbl)
+
+    -- Case 2: table exists and faction is in it, so add/overwrite the siege entry
+    elseif cur_siege_tbl[f] ~= nil then
+        Log(f, "is already in cur_siege_tbl")
+        cur_siege_tbl[f][besieger_cqi] = { ["turn_started"] = turn_num, ["target_region"] = region:name() }
+        cm:set_saved_value('jar_siege_tbl', cur_siege_tbl)
+    -- Case 3: table exists and faction is not in it, so add the faction and siege entry
+    else
+        Log(f, "is not in cur_siege_tbl")
+        cur_siege_tbl[f] = { [besieger_cqi] = { ["turn_started"] = turn_num, ["target_region"] = region:name() } }
+        cm:set_saved_value('jar_siege_tbl', cur_siege_tbl)
+    end
+end
+
+local function handle_faction_force_siege(faction)
+    Log("[FUNC] handle_faction_force_siege", faction:name())
+    local f = faction:name()
+    local cur_turn = cm:turn_number()
+    local n = settings.force_siege_assault_turns
+    local siege_tbl = cm:get_saved_value('jar_siege_tbl')
+    local f_entry = siege_tbl[f]
+    if faction:is_dead() then
+        Log("faction is dead, removing entry")
+        f_entry = nil
+    end
+    for key, value in pairs(f_entry) do
+        local besieger_char = cm:get_character_by_cqi(key)
+        if (not besieger_char) or (besieger_char == nil) or (is_null(besieger_char)) or (not besieger_char:is_besieging()) then
+            Log("character with cqi", key, "is no longer besieging", value.target_region)
+            f_entry[key] = nil
+            if is_empty_table(f_entry) then
+                f_entry = nil
+            end
+        elseif (cur_turn - value.turn_started) >= n then
+            Log("siege length has met threshold:", cur_turn - value.turn_started, ">=", n)
+            local besieger_lookup_str = cm:char_lookup_str(key)
+            local r = value.target_region
+            local reg = cm:get_region(r)
+            Log("healing garrison")
+            cm:heal_garrison(reg:cqi())
+            local g_cmd = cm:get_garrison_commander_of_region(reg)
+            if g_cmd == nil or g_cmd:is_null_interface() then
+                Log("garrison commander is still nil, cannot force attack")
+            else
+                local g_cmd_lookup_str = cm:char_lookup_str(g_cmd)
+                Log("forcing siege assault")
+                cm:attack(besieger_lookup_str, g_cmd_lookup_str, false, true)
+            end
+        end
+    end
+    cm:set_saved_value('jar_siege_tbl', siege_tbl)
+end
+
 -- LuaFormatter off
 core:add_listener(
     "JarAdjArmiesCharSelected",
@@ -404,6 +474,44 @@ core:add_listener(
         Log("[LSTR] JarAdjArmiesCharAncGained")
         local hero = context:character()
         refresh_army_with_hero(hero:embedded_in_military_force():general_character(), hero)
+    end,
+    true
+)
+
+core:add_listener(
+    "JarAdjArmiesCharBesieges",
+    "CharacterBesiegesSettlement",
+    function(context)
+        return (
+            not context:region():garrison_residence():besieging_character():faction():is_human()
+            and settings.force_siege_assault
+        )
+    end,
+    function(context)
+        Log("[LSTR] JarAdjArmiesCharBesieges")
+        local r = context:region()
+        handle_char_besieges(r)
+    end,
+    true
+)
+
+core:add_listener(
+    "JarAdjArmiesFactionTurnNormal",
+    "FactionBeginTurnPhaseNormal",
+    function(context)
+        local f = context:faction():name()
+        local siege_tbl = cm:get_saved_value('jar_siege_tbl')
+        return (
+            (siege_tbl ~= nil)
+            and (siege_tbl[f] ~= nil)
+            and (not is_empty_table(siege_tbl[f]))
+            and (settings.force_siege_assault)
+        )
+    end,
+    function(context)
+        Log("[LSTR] JarAdjArmiesFactionTurnNormal")
+        local f = context:faction()
+        handle_faction_force_siege(f)
     end,
     true
 )
